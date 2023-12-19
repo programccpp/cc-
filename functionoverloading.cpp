9c8d8547af6fa0b625b55a006b8c599c4dcfd02f
programccpp <154233367+programccpp@users.noreply.github.com>
2023-12-19 06:18:55 +0530

    Add files via upload

---
+++ functionoverloading.cpp
@@ -0,0 +1,18 @@
+#include <iostream.h>
+void add(int a, int b)
+{
+cout << "sum = " << (a + b);
+}
+
+void add(double a, double b)
+{
+	cout << endl << "sum = " << (a + b);
+}
+
+int main()
+{
+	add(10, 2);
+	add(5.3, 6.2);
+
+	return 0;
+}
